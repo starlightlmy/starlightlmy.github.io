405d128a630101b393351da603edb60287497032
starlightlmy <2573545674@qq.com>
2023-09-01 12:16:31 +0800

    static-web

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,14 @@
 module.exports = {
     title: 'Hello VuePress',
-    description: 'Just playing around'
+    description: 'Just playing around',
+    themeConfig: {
+        series: {
+          '/vuepress-theme-reco/': [ 'introduce', 'usage' ]
+        },
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    }
 }
